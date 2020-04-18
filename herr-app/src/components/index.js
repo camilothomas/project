36ad56043922748b6572b413e2289945feaa0461
EmmanuelZapata <zapata.emmanuel@gmail.com>
2020-04-17 23:30:23 -0500

    Changed location of components to proper component folder. Changed the import on App.js to follow those changes.

--- herr-app/src/components/index.js
+++ herr-app/src/components/index.js
@@ -1,1 +1,3 @@
-export { default as Blob } from './Blob'
+export { default as Blob } from "./Blob";
+export { default as Circle } from "./Circle";
+export { default as GoldenRectangle } from "./GoldenRectangle";
