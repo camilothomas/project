a846922b99a8a6cec5ddabda7a4bbc7fee021d50
camilothomas <cembre@gmail.com>
2020-04-22 23:26:59 -0400

    introduces simple incomplete grid

---
+++ herr-app/src/components/Grid/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    justify-items: stretch;
+    align-items: stretch;
+`;
+
+export default Grid;
